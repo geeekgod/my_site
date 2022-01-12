8fbdfe1ad2e79035b450ad8b5d6d6f2efba209b0
Rishabh Singh <ris8645208@gmail.com>
2022-01-12 14:04:28 +0530

    fix: update props values of svg

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -13,8 +13,8 @@ const Navbar: React.FC = () => {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               strokeWidth="2"
               d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             ></path>
