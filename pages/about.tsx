31c00e5f381b380a0ea944235665ef05ccef1872
Rishabh Singh <ris8645208@gmail.com>
2022-01-13 14:34:01 +0530

    feat: add animations & about page update

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,39 +13,84 @@ const About = () => {
         />
       </Head>
       <div>
-        <div className="flex items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          className="flex items-center justify-center"
+        >
           <h3 className="text-3xl font-semibold mt-7 border-black dark:border-white pb-4 border-b-4">
             About Me
           </h3>
-        </div>
+        </motion.div>
         <div className="flex mx-10 flex-col  py-10 px-6  mt-4 mb-6  slg:mx-12 slg:flex-row">
-          <div className="flex items-center justify-center slg:w-2/4 p-6">
-            <img src={"/assets/myimage.png"} className="w-56 slg:w-72" />
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 200 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.7 } }}
+            className="flex items-center justify-center slg:w-2/4 p-6"
+          >
+            <motion.img
+              whileHover={{ scale: 1.07, transition: { type: "spring" } }}
+              src={"/assets/myimage.png"}
+              className="w-56 slg:w-72 cursor-pointer border-[6px] rounded-full bg-[#4262F0] border-[#4262F0] dark:bg-[#7C92F4] dark:border-[#7C92F4]"
+            />
+          </motion.div>
           <div className="flex flex-col justify-center slg:w-2/4">
-            <h3 className="text-3xl my-2 font-semibold">Rishabh Singh</h3>
-            <h3 className="text-xl my-1">Full-Stack Developer</h3>
-            <h3 className="text-lg my-1 opacity-80">
+            <motion.h3
+              initial={{ opacity: 0, x: -70 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", delay: 0.6 }}
+              className="text-3xl my-2 font-semibold"
+            >
+              Rishabh Singh
+            </motion.h3>
+            <motion.h3
+              initial={{ opacity: 0, x: -70 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", delay: 0.8 }}
+              className="text-xl my-1"
+            >
+              Full-Stack Developer
+            </motion.h3>
+            <motion.h3
+              initial={{ opacity: 0, x: -70 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", delay: 1 }}
+              className="text-lg my-1 opacity-80"
+            >
               Based out in Mumbai, India
-            </h3>
-            <p className="text-md my-3 text-gray-600 dark:text-slate-400">
-              I started programming back when I was at 14 , trying to use html
-              and develop basic games & flex among my friends, but eventually,
-              this passion evolved into today what i'm.
-            </p>
-            <p className="text-md my-3 text-gray-600 dark:text-slate-400">
-              Currently working as a Web Developer at{" "}
-              <motion.a
-                whileHover={{ scale: 1.06 }}
+            </motion.h3>
+            <motion.p
+              initial={{ opacity: 0, y: 150 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", duration: 0.6, delay: 0.9 }}
+              className="text-md my-3 text-gray-600 dark:text-slate-400"
+            >
+              I started programming back when I was at 14 , trying to use basic
+              programming languages & develop basic games & flex among my
+              friends, but eventually,
+              <br /> this passion evolved into today what I'm.
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, y: 150 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", duration: 0.6, delay: 1.3 }}
+              className="text-md my-3 text-gray-600 dark:text-slate-400"
+            >
+              Currently pursuing my bachelor's in Computer Engineering & working
+              as a Web Developer at{" "}
+              <a
                 className="dark:text-[#7C92F4] text-[#4262F0] font-semibold border-b-2 dark:border-[#7C92F4] border-[#4262F0] "
                 href="https://ssingularity.co.in/"
                 target="_blank"
                 rel="noreferrer"
               >
-                Ssingularity Tech
-              </motion.a>
-              .{"\n"}
-            </p>
+                SSingularity Tech
+              </a>
+              .<br />I love creating things that revolve around web, be it
+              Websites, Web Apps, Mobile Apps. I've spent 2 years working on web
+              technologies & make my work pixel perfect with every new project I
+              work on.
+            </motion.p>
           </div>
         </div>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,17 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>Rishabh Singh</title>
-        <meta name="description" content="Rishabh Singh Full-Stack Developer. Welcome to my website" />
+        <meta
+          name="description"
+          content="Rishabh Singh Full-Stack Developer. Welcome to my website"
+        />
       </Head>
 
       <div className="flex mx-10 flex-col  py-10 px-6 rounded-md mt-4 mb-6 bg-slate-100 shadow-sm dark:bg-gray-800 slg:mx-12 slg:flex-row  slg:shadow-none slg:dark:bg-transparent slg:bg-transparent">
         <motion.div
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}
           className="imgcontainer flex items-center justify-center slg:w-2/4"
         >
           <img src={"/assets/Development-cuate.png"} className="w-full" />
@@ -22,6 +26,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.6, type: "spring", duration: 0.6 }}
             className="text-primary font-bold text-3xl my-2"
           >
             {"<Rishabh>"}
@@ -29,6 +34,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.8, type: "spring", duration: 0.6 }}
             className="font-bold text-3xl my-2 ml-5 dark:text-white"
           >
             {"Full-Stack Developer"}
@@ -36,6 +42,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 1, type: "spring", duration: 0.6 }}
             className="text-primary font-bold text-3xl my-2"
           >
             {"</Rishabh>"}
@@ -43,6 +50,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { delay: 0.2, duration: 1 } }}
+            transition={{ delay: 0.8, type: "spring", duration: 0.6 }}
             className="font-medium text-md my-2 ml-5 dark:text-white"
           >
             {"Developing quality software to"}
@@ -53,6 +61,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.9, type: "spring", duration: 0.6 }}
             className="my-4 mx-5"
           >
             <motion.button
