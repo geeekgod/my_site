d46706b3317d892661f229cf3723b4629c902f6d
Rishabh Singh <ris8645208@gmail.com>
2025-03-13 23:15:46 +0530

    chore: update next-themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
