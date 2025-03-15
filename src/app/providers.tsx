b8bead1c9b7e10f0cf69be075274a1bde39473ea
Rishabh Singh <ris8645208@gmail.com>
2025-03-15 23:25:00 +0530

    fix: build issues

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,7 +5,12 @@ import { ReactNode } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       {children}
     </ThemeProvider>
   );
